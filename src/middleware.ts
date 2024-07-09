8f87ff113efeb0fd246b2b6b61f512655e44d229
Babina081 <babitamoo333@gmail.com>
2024-07-09 18:21:54 +0545

    add the verifyemail route in the middleware for authorization

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,8 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const isPublicPath = path === "/login" || path === "/signup";
+  const isPublicPath =
+    path === "/login" || path === "/signup" || path === "/verifyemail";
   const token = request.cookies.get("token")?.value || "";
   if (token && isPublicPath) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
@@ -11,10 +12,8 @@ export function middleware(request: NextRequest) {
   if (!isPublicPath && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
-
-  
 }
 
 export const config = {
-  matcher: ["/", "/profile", "/login", "/signup"],
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"],
 };
