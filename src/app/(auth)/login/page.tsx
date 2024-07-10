89f9835c89f6ab1ffd517b35fb088d4e080021ee
Babina081 <babitamoo333@gmail.com>
2024-07-10 12:19:49 +0545

    update login and signup form design

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,8 +2,9 @@
 import Link from "next/link";
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import axios  from "axios";
+import axios from "axios";
 import toast from "react-hot-toast";
+import Cookies from "js-cookie";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -13,14 +14,29 @@ export default function LoginPage() {
   });
   const [buttonDisabled, setButtonDisabled] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+  const [rememberMe, setRememberMe] = React.useState(false);
+
+  const handleRememberMeChange = (e: any) => {
+    setRememberMe(e.target.current);
+  };
 
   const onLogin = async () => {
     try {
       setLoading(true);
       const response = await axios.post("/api/users/login", user);
-      console.log("Login success", response.data);
-      toast.success("Login success");
-      router.push("/profile");
+      console.log(response);
+
+      if (response) {
+        console.log("Login success", response.data);
+        toast.success("Login success");
+        // Set the user email in the cookie
+        if (rememberMe) {
+          Cookies.set("user-email", user.email);
+        } else {
+          Cookies.remove("user-email");
+        }
+        router.push("/profile");
+      }
     } catch (error: any) {
       console.log("Login Failed: " + error.message);
       toast.error(error.message);
@@ -38,38 +54,58 @@ export default function LoginPage() {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <h1 className="text-center text-white text-2xl ">
-        {loading ? "Loading..." : "Login"}
-      </h1>
-      <hr />
+    <div className="flex flex-col items-center justify-center h-full py-2  ">
+      <form action="" className="flex flex-col justify-start items-start ">
+        <h1 className=" text-white text-2xl font-bold mb-4">
+          {" "}
+          {loading ? "Loading..." : "Log In"}
+        </h1>
+        <hr />
 
-      <label htmlFor="email">Email</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        type="text"
-        id="email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        type="text"
-        id="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      />
+        <input
+          className="w-full p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:border-gray-600 text-black placeholder:text-sm"
+          type="text"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
 
-      <button
-        onClick={onLogin}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-      >
-        {buttonDisabled ? "Login disabled" : "Login"}
-      </button>
-      <Link href="/signup">Visit signup page</Link>
+        <input
+          className="w-full p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:border-gray-600 text-black placeholder:text-sm"
+          type="text"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+        <div className="flex gap-1 mb-2 w-full text-left items-center ">
+          <input
+            type="checkbox"
+            id="remember-me"
+            className="h-4 w-4 "
+            onChange={handleRememberMeChange}
+            checked={rememberMe}
+          />
+          <label htmlFor="remember-me">Remember Me</label>
+        </div>
+
+        <button
+          onClick={onLogin}
+          className="w-full p-2 bg-gradient-radial  from-pink-500 to-purple-500 hover:from-pink-600 hover:to-purple-600 rounded-lg mb-2 focus:outline-none focus:border-gray-600 "
+        >
+          {buttonDisabled ? "Login disabled" : "Login"}
+        </button>
+        <p>
+          Don&apos;t have an account yet?{" "}
+          <Link
+            href="/signup"
+            className="text-pink-500 hover:underline hover:text-pink-300 transition-shadow duration-500 ease-in-out delay-150"
+          >
+            Sign up!
+          </Link>
+        </p>
+      </form>
     </div>
   );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -44,46 +44,63 @@ export default function SignUpPage() {
   }, [user]);
 
   return (
-    <div className="flex flex-col items-center justify-center ">
-      <h1 className="text-center text-white text-2xl ">
-        {loading ? "Loading...." : "SignUp"}
-      </h1>
-      <hr />
-      <label htmlFor="username">Username</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        type="text"
-        id="username"
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-        placeholder="username"
-      />
-      <label htmlFor="email">Email</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        type="text"
-        id="email"
-        value={user.email}
-        onChange={(e) => setUser({ ...user, email: e.target.value })}
-        placeholder="email"
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-        type="text"
-        id="password"
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-        placeholder="password"
-      />
+    <div className="flex flex-col items-center justify-center h-full py-2">
+      <form action="" className="flex flex-col justify-start items-start ">
+        <h1 className=" text-white text-2xl font-bold mb-4">
+          {loading ? "Loading...." : "Sign Up"}
+        </h1>
+        <hr />
 
-      <button
-        onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
-      >
-        {buttonDisabled ? "Signup Disabled" : "Signup"}
-      </button>
-      <Link href="/login">Visit login page</Link>
+        <input
+          className="w-full p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:border-gray-600 text-black  placeholder:text-sm
+ "
+          type="text"
+          id="username"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder="Username"
+        />
+
+        <input
+          className="w-full p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:border-gray-600 text-black  placeholder:text-sm
+ "
+          type="text"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
+
+        <input
+          className="w-full p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none focus:border-gray-600 text-black  placeholder:text-sm
+ "
+          type="text"
+          id="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
+
+        <label htmlFor="forgot-password" className="mb-2 w-full text-right">
+          <Link href="/forgot-password">Forgot password?</Link>
+        </label>
+
+        <button
+          onClick={onSignup}
+          className="w-full p-2 bg-gradient-radial  from-pink-500 to-purple-500 hover:from-pink-600 hover:to-purple-600 rounded-lg mb-2 focus:outline-none focus:border-gray-600 "
+        >
+          {buttonDisabled ? "Signup Disabled" : "Signup"}
+        </button>
+        <p>
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="text-pink-500 hover:underline hover:text-pink-300 transition-shadow duration-500 ease-in-out delay-150"
+          >
+            Login Now!
+          </Link>
+        </p>
+      </form>
     </div>
   );
 }
