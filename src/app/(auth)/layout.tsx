5a7f09e0739e2b368132ca13d433b450c9bd07c1
Babina081 <babitamoo333@gmail.com>
2024-07-10 23:23:48 +0545

    update text color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gradient-to-br from-red-300 to-indigo-600 h-[100vh] overflow-hidden  flex justify-center items-center ">
+    <div className="bg-gradient-to-br from-red-300 to-indigo-600 h-[100vh] overflow-hidden  flex justify-center items-center text-white">
       <div className="  md:rounded-3xl  h-full w-full md:h-auto md:w-auto  md:max-w-7xl shadow-[0px_0px_100px_10px_rgba(0,0,0,0.30)] bg-gradient-to-br from-red-300 to-indigo-600 p-10 grid grid-cols-1  md:grid-cols-2 ">
         <div className="hidden md:flex flex-col items-center justify-start p-1 ">
           <h1 className="text-5xl font-bold border-b-4  border-white mb-4">
