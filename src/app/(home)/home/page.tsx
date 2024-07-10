4b4a95eba6bf288f844c53286b4aef887e668acb
Babina081 <babitamoo333@gmail.com>
2024-07-10 23:23:02 +0545

    add home page route

---
+++ src/app/(home)/home/page.tsx
@@ -0,0 +1,16 @@
+import About from "@/components/home/About";
+import Banner from "@/components/home/Banner";
+import Category from "@/components/home/Category";
+
+const HomePage = () => {
+  return (
+    <>
+      <Banner></Banner>
+      <About></About>
+      <Category></Category>
+      <Banner></Banner>
+    </>
+  );
+};
+
+export default HomePage;
