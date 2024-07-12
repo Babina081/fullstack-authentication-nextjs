340f43fdd75e5e5493e7f1aa944588014bea050d
Babina081 <babitamoo333@gmail.com>
2024-07-12 22:11:52 +0545

    add gsap ui for category display

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,63 +1,81 @@
-import Image from "next/image";
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Category = () => {
+  // const heroRightRef = useRef<HTMLDivElement>(null);
+  // const itemsHeight = heroRightRef?.current?.clientHeight || 0;
+  // const items = gsap.utils.toArray(".hero__item") as HTMLElement[];
+
+  // useEffect(() => {
+  //   gsap.to(".hero__right", {
+  //     y:  itemsHeight - 400,
+  //     x: innerWidth,
+  //     ease: "none",
+  //     scrollTrigger: {
+  //       trigger: ".hero",
+  //       start: "top top",
+  //       end:-itemsHeight + 400,
+  //       scrub: 1,
+  //       markers: true,
+  //       pin: true,
+  //       pinSpacing: false,
+  //       snap: {
+  //         snapTo: 1 / (items.length - 1),
+  //         duration: 0.2,
+  //         delay: 0,
+  //       },
+  //     },
+  //   });
+
+  //   items.forEach((section) => {
+  //     gsap.from(section, {
+  //       scrollTrigger: {
+  //         markers: true,
+  //         start: "top 50%",
+  //         end: "bottom 50%",
+  //         trigger: section,
+  //         toggleClass: "active",
+  //       },
+  //     });
+  //   });
+  // }, []);
+
   return (
-    <>
-      <section className="categorySection h-screen bg-purple-400 justify-center px-14 py-8 relative w-full bg-gradient-to-br from-pink-300 to-indigo-600 overflow-x-clip">
-        <div className="grid grid-cols-2 bg-yellow-100 h-full absolute w-full ">
-          <div className="flex flex-col  justify-center md:justify-start items-center md:items-start px-10">
-            {" "}
-            <h4 className="text-xl my-4 text-pink-700 font-bold  border-b-2 border-b-pink-700">
-              THE BEST WE HAVE
-            </h4>{" "}
-            <h1 className="text-xl md:text-3xl py-0 md:py-2 text-purple-600  font-extrabold ">
-              Our Products Category
-            </h1>
-            <p className="text-xs md:text-sm py-0 md:py-2 text-purple-600  font-bold ">
-              True elegance lies in the harmonious blend of simplicity and
-              sophistication, where each garment speaks of timeless beauty and
-              effortless style. From the subtle grace of flowing fabrics to the
-              bold statement of tailored lines, our collection embodies a
-              symphony of textures and colors that resonate with individuality
-              and grace. Embrace the artistry of fashion that transcends trends,
-              embracing every moment with poise and confidence.
-            </p>
-            <Image
-              src="/dress.png"
-              width={400}
-              height={400}
-              alt="Picture of the cloth1"
-            />
-            <Image
-              src="/shoes.png"
-              width={400}
-              height={400}
-              alt="Picture of the cloth1"
-            />
-            <Image
-              src="/formal.png"
-              width={400}
-              height={400}
-              alt="Picture of the cloth1"
-            />{" "}
-            <Image
-              src="/casual.png"
-              width={400}
-              height={400}
-              alt="Picture of the cloth1"
-            />{" "}
-            <Image
-              src="/bag.png"
-              width={400}
-              height={400}
-              alt="Picture of the cloth1"
-            />
-          </div>
-          <div className="">world</div>
+    <section className="hero h-screen min-h-[500px]  bg-purple-400 flex items-center text-red-500 ">
+      <div className="hero-wrap h-[400px] w-full bg-yellow-100 flex p-5 relative">
+        <div className="hero__left col w-[50vw] ">
+          <h3 className="text-5xl">Choose your favorite fruit</h3>
         </div>
-      </section>
-    </>
+
+        <div
+          className="hero__right col absolute  w-[50vw] right-5 h-full pl-5 z-10"
+        >
+          <div className="hero__item">
+            <div className="item__name">Apple</div>
+            <div className="item__cost">01</div>
+          </div>
+          <div className="hero__item">
+            <div className="item__name">Bananna</div>
+            <div className="item__cost">02</div>
+          </div>
+          <div className="hero__item">
+            <div className="item__name">Kiwi</div>
+            <div className="item__cost">03</div>
+          </div>
+          <div className="hero__item">
+            <div className="item__name">Orange</div>
+            <div className="item__cost">04</div>
+          </div>
+          <div className="hero__item">
+            <div className="item__name">Pear</div>
+            <div className="item__cost">05</div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
