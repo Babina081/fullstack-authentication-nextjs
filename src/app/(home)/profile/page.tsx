e50c89b7deb764e788f9c8cdd3a4966a49dace07
Babina081 <babitamoo333@gmail.com>
2024-07-12 22:09:15 +0545

    add profile page content and moved the file to the general directory

---
+++ src/app/(home)/profile/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Sidebar from "@/components/profile/Sidebar";
+type Users = { username: String; email: String; _id: String; password: String };
+
+export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("nothing");
+  const [users, setUsers] = useState<Users[]>([]);
+
+ const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+
+    setData(res.data.data._id);
+    console.log(res.data.data._id);
+  };
+
+  const fetchUsers = async () => {
+    const response = await axios.get("/api/users");
+    setUsers(response.data.data);
+    console.log(response.data.data);
+  };
+
+  const deleteUser = async (id: any) => {
+    try {
+      const response = await axios.delete(`/api/users/deleteusers?id=${id}`);
+      toast.success("User deleted successfully");
+      fetchUsers();
+    } catch (error: any) {
+      console.log("Deletion Failed: ", error.message);
+      toast.error("Failed to delete user");
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <section className="w-full h-screen px-14 py-4 bg-gradient-to-br  from-red-300 to-indigo-600  profileSection flex flex-col text-purple-600 ">
+      <h1 className="text-xl md:text-2xl font-bold  text-center md:text-left py-2  mr-0 md:mr-24 w-full text-purple-600 ">
+        Profile
+      </h1>
+
+      {/* sidebar */}
+     <Sidebar logout={logout}></Sidebar>
+
+      {/* --------------- */}
+      <div className="flex  items-center justify-evenly min-h-screen gap-6">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+          <h1>Profile</h1>
+          <hr />
+          <p>Profile Page</p>
+          <h2 className="p-3 rounded bg-green-500">
+            {data === "nothing" ? (
+              "Nothing"
+            ) : (
+              <Link href={`/profile/${data}`}>{data}</Link>
+            )}
+          </h2>
+          <button
+            onClick={getUserDetails}
+            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Get User Details
+          </button>
+          <hr />
+          <button
+            onClick={logout}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Logout
+          </button>
+        </div>
+        <div className="text-white">
+          <ul className="border-2 border-white border-dashed m-2 p-2 ">
+            <h1 className="border-b-2 border-white w-auto mb-4 text-center font-bold">
+              List of Users
+            </h1>
+            {users.map((user, i) => {
+              return (
+                <li
+                  key={String(user._id)}
+                  className="flex justify-between items-center gap-6"
+                >
+                  <h2>
+                    {i + 1} : {user.username}
+                  </h2>
+                  <button
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => deleteUser(user._id)}
+                  >
+                    Delete user
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
