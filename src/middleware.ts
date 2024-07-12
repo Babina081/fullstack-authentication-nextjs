5717b9d5e1fb0b9be501ccd22e46333303906a88
Babina081 <babitamoo333@gmail.com>
2024-07-12 22:07:14 +0545

    add matcher url

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"],
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyemail", "/home"],
 };
