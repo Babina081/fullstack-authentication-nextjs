d4d10f186b349242b44e04a555dc51f187fa01c0
Babina081 <babitamoo333@gmail.com>
2024-07-13 22:32:11 +0545

    add category skeleton shimmer component

---
+++ src/SkeletonShimmer/CategoryShimmer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CategoryShimmer = () => {
+  return (
+    <div className="card relative bg-gray-400 animate-pulse">
+           
+          </div>
+  )
+}
+
+export default CategoryShimmer
