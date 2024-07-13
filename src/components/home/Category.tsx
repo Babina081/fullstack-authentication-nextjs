454e9b6541c910a34a71301456265fba0b127936
Babina081 <babitamoo333@gmail.com>
2024-07-13 22:30:12 +0545

    update category section ui and dynamic category fetch

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,82 +1,126 @@
 "use client";
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
+import axios from "axios";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
 
-const Category = () => {
-  // const heroRightRef = useRef<HTMLDivElement>(null);
-  // const itemsHeight = heroRightRef?.current?.clientHeight || 0;
-  // const items = gsap.utils.toArray(".hero__item") as HTMLElement[];
+const Products = () => {
+  const router = useRouter();
+  const stackRef = useRef<HTMLDivElement | null>(null);
+  const [category, setCategory] = useState([]);
+  const [loading, setLoading] = React.useState(false);
 
-  // useEffect(() => {
-  //   gsap.to(".hero__right", {
-  //     y:  itemsHeight - 400,
-  //     x: innerWidth,
-  //     ease: "none",
-  //     scrollTrigger: {
-  //       trigger: ".hero",
-  //       start: "top top",
-  //       end:-itemsHeight + 400,
-  //       scrub: 1,
-  //       markers: true,
-  //       pin: true,
-  //       pinSpacing: false,
-  //       snap: {
-  //         snapTo: 1 / (items.length - 1),
-  //         duration: 0.2,
-  //         delay: 0,
-  //       },
-  //     },
-  //   });
+  const getCategory = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/categories"
+      );
+      setCategory(response.data);
+    } catch (error: any) {
+      console.log({ error: error.message });
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  //   items.forEach((section) => {
-  //     gsap.from(section, {
-  //       scrollTrigger: {
-  //         markers: true,
-  //         start: "top 50%",
-  //         end: "bottom 50%",
-  //         trigger: section,
-  //         toggleClass: "active",
-  //       },
-  //     });
-  //   });
-  // }, []);
+  useEffect(() => {
+    getCategory();
+  }, []);
+
+  useEffect(() => {
+    if (stackRef.current) {
+      const stack = stackRef.current;
+      const cards = Array.from(stack.children)
+        .reverse()
+        .filter((child): child is HTMLDivElement =>
+          child.classList.contains("card")
+        );
+
+      cards.forEach((card) => stack.appendChild(card));
+
+      const moveCard = () => {
+        const lastCard = stack.lastElementChild as HTMLDivElement | null;
+        if (lastCard && lastCard.classList.contains("card")) {
+          lastCard.classList.add("swap");
+
+          setTimeout(() => {
+            lastCard.classList.remove("swap");
+            stack.insertBefore(lastCard, stack.firstElementChild);
+          }, 1200);
+        }
+      };
+
+      let autoplayInterval = setInterval(moveCard, 4000);
+
+      stack.addEventListener("click", function (e: any) {
+        const card = e.target.closest(".card") as HTMLDivElement | null;
+        if (card && card === stack.lastElementChild) {
+          card.classList.add("swap");
+
+          setTimeout(() => {
+            card.classList.remove("swap");
+            stack.insertBefore(card, stack.firstElementChild);
+          }, 1200);
+        }
+      });
+
+      return () => {
+        clearInterval(autoplayInterval);
+        stack.removeEventListener("click", moveCard);
+      };
+    }
+  }, []);
 
   return (
-    <section className="hero h-screen min-h-[500px]  bg-purple-400 flex items-center text-red-500 ">
-      <div className="hero-wrap h-[400px] w-full bg-yellow-100 flex p-5 relative">
-        <div className="hero__left col w-[50vw] ">
-          <h3 className="text-5xl">Choose your favorite fruit</h3>
-        </div>
-
-        <div
-          className="hero__right col absolute  w-[50vw] right-5 h-full pl-5 z-10"
+    <main className="main-products">
+      <div className="content">
+        <h4 className="text-xl my-4 text-pink-700 font-bold  border-b-2 border-b-pink-700">
+          THE BEST WE HAVE
+        </h4>{" "}
+        <h1 className="text-xl md:text-3xl py-0 md:py-2 text-purple-600  font-extrabold ">
+          Our Products Category
+        </h1>
+        <p className="text-xs md:text-sm py-0 md:py-2 text-purple-600  font-bold ">
+          True elegance lies in the harmonious blend of simplicity and
+          sophistication, where each garment speaks of timeless beauty and
+          effortless style. From the subtle grace of flowing fabrics to the bold
+          statement of tailored lines, our collection embodies a symphony of
+          textures and colors that resonate with individuality and grace.
+          Embrace the artistry of fashion that transcends trends, embracing
+          every moment with poise and confidence.
+        </p>
+        <button
+          className="btn"
+          onClick={() => {
+            router.push("/shop");
+          }}
         >
-          <div className="hero__item">
-            <div className="item__name">Apple</div>
-            <div className="item__cost">01</div>
-          </div>
-          <div className="hero__item">
-            <div className="item__name">Bananna</div>
-            <div className="item__cost">02</div>
-          </div>
-          <div className="hero__item">
-            <div className="item__name">Kiwi</div>
-            <div className="item__cost">03</div>
-          </div>
-          <div className="hero__item">
-            <div className="item__name">Orange</div>
-            <div className="item__cost">04</div>
-          </div>
-          <div className="hero__item">
-            <div className="item__name">Pear</div>
-            <div className="item__cost">05</div>
-          </div>
-        </div>
+          Explore More
+        </button>
       </div>
-    </section>
+
+      <div className="stack" ref={stackRef}>
+        {category.map((item: any) => (
+          <div className="card relative cursor-pointer" key={item._id}>
+            <Image
+              loader={() => item.image}
+              width={100}
+              height={100}
+              src={item.image}
+              alt={item.name}
+              className="img h-full w-full object-cover"
+            ></Image>
+            <div className="absolute bottom-[50%] left-[50%] transform   translate-x-[-50%] translate-y-[-50%]   item__name text-xl md:text-3xl font-bold text-center uppercase bg-gradient-to-r from-[#f76591] to-[#ffc16f] bg-clip-text text-transparent  ">
+              {item.name}
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 };
 
-export default Category;
+export default Products;
