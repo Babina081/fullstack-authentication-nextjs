e66d4c4ef0dbe840b05d209bc6df6aa56d92ffc0
Babina081 <babitamoo333@gmail.com>
2024-07-13 22:20:36 +0545

    update format

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,9 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const isPublicPath =
-    path === "/login" || path === "/signup" || path === "/verifyemail";
+    path === "/login" ||
+    path === "/signup" ||
+    path === "/verifyemail" 
   const token = request.cookies.get("token")?.value || "";
   if (token && isPublicPath) {
     return NextResponse.redirect(new URL("/home", request.nextUrl));
