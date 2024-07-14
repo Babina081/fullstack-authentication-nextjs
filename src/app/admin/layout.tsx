926b8270c0880c6a4d680196cfb2b5770da8f870
Babina081 <babitamoo333@gmail.com>
2024-07-14 17:02:43 +0545

    update layout of admin page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,9 +3,11 @@ import AdminNavbar from "@/components/admin/AdminNavbar";
 import React from "react";
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex w-4/4 h-auto">
-      <AdminSidebar/>
-      <div className="flex flex-col w-3/4">
+    <main className="flex w-4/4 h-auto relative">
+      <div className="w-1/4 fixed top-0 left-0">
+        <AdminSidebar />
+      </div>
+      <div className="flex flex-col w-3/4 fixed top-0 right-0">
         <AdminNavbar></AdminNavbar>
         {children}
       </div>
