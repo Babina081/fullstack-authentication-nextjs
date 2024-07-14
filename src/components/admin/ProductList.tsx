765b66b8b214664867c649e1ad0ba89728a64c84
Babina081 <babitamoo333@gmail.com>
2024-07-14 17:06:00 +0545

    add productlist component

---
+++ src/components/admin/ProductList.tsx
@@ -0,0 +1,426 @@
+"use client";
+import DeleteEditButton from "@/components/admin/DeleteEditButton";
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import toast from "react-hot-toast";
+
+interface Product {
+  name: string;
+  category: { name: string };
+  brand: string;
+  color: string;
+  icon: string;
+  image: string;
+  isFeatured: boolean;
+  price: number;
+  rating: number;
+  numReviews: number;
+  inStock: number;
+  richDescription: string;
+  images: string[];
+}
+
+const ProductList = ({ token }: any) => {
+  const [products, setProducts] = React.useState<Product[]>([]);
+  const [categories, setCategories] = React.useState<Product[]>([]);
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/api/v1/products");
+      const products = response.data;
+      setProducts(products);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/categories"
+      );
+      const categories = response.data;
+      setCategories(categories);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+    fetchCategories();
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-between w-full mb-2 items-center">
+        <h1 className="text-xl font-bold sticky top-0 left-0">Products</h1>
+        {/* search and filter */}
+        <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4bg-pink-100">
+          {/* category filter*/}
+
+          <div>
+            <button
+              id="dropdownRadioButton"
+              data-dropdown-toggle="dropdownRadio"
+              className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              type="button"
+            >
+              <svg
+                className="w-3 h-3 text-gray-500 dark:text-gray-400 me-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm3.982 13.982a1 1 0 0 1-1.414 0l-3.274-3.274A1.012 1.012 0 0 1 9 10V6a1 1 0 0 1 2 0v3.586l2.982 2.982a1 1 0 0 1 0 1.414Z" />
+              </svg>
+              Last 30 days
+              <svg
+                className="w-2.5 h-2.5 ms-2.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+
+            <div
+              id="dropdownRadio"
+              className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+              data-popper-reference-hidden=""
+              data-popper-escaped=""
+              data-popper-placement="top"
+              style={{
+                position: "absolute",
+                inset: "auto auto 0px 0px",
+                margin: "0px",
+                transform: "translate3d(522.5px, 3847.5px, 0px)",
+              }}
+            >
+              <ul
+                className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200"
+                aria-labelledby="dropdownRadioButton"
+              >
+                <li>
+                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <input
+                      id="filter-radio-example-1"
+                      type="radio"
+                      value=""
+                      name="filter-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="filter-radio-example-1"
+                      className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                    >
+                      Last day
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <input
+                      defaultChecked={true}
+                      id="filter-radio-example-2"
+                      type="radio"
+                      value=""
+                      name="filter-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="filter-radio-example-2"
+                      className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                    >
+                      Last 7 days
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <input
+                      id="filter-radio-example-3"
+                      type="radio"
+                      value=""
+                      name="filter-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="filter-radio-example-3"
+                      className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                    >
+                      Last 30 days
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <input
+                      id="filter-radio-example-4"
+                      type="radio"
+                      value=""
+                      name="filter-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="filter-radio-example-4"
+                      className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                    >
+                      Last month
+                    </label>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <input
+                      id="filter-radio-example-5"
+                      type="radio"
+                      value=""
+                      name="filter-radio"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor="filter-radio-example-5"
+                      className="w-full ms-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"
+                    >
+                      Last year
+                    </label>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* search items */}
+          <label htmlFor="table-search" className="sr-only">
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 rtl:inset-r-0 rtl:right-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-5 h-5 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </div>
+            <input
+              type="text"
+              id="table-search"
+              className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search for items"
+            />
+          </div>
+          <Link
+            href="/admin/products/add"
+            className="bg-green-400 p-2 text-sm text-white font-bold rounded-lg"
+          >
+            + Add Products
+          </Link>
+        </div>
+      </div>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg w-full mb-8 h-96">
+        {/* table */}
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 relative ">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0 left-0 z-20">
+            <tr>
+              <th scope="col" className="p-4">
+                <div className="flex items-center">
+                  <input
+                    id="checkbox-all-search"
+                    type="checkbox"
+                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                  />
+                  <label htmlFor="checkbox-all-search" className="sr-only">
+                    checkbox
+                  </label>
+                </div>
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Image
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Brand
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Stock
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {products.map((product: any, i: any) => (
+              <tr
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                key={i}
+              >
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input
+                      id={`checkbox-table-search-${i}`}
+                      type="checkbox"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                    />
+                    <label
+                      htmlFor={`checkbox-table-search-${i}`}
+                      className="sr-only"
+                    >
+                      checkbox
+                    </label>
+                  </div>
+                </td>
+                <td className="px-6 py-4">
+                  <Image
+                    src={
+                      typeof product?.image === "string" ? product.image : ""
+                    }
+                    alt={product?.name || "Product Image"}
+                    height={100}
+                    width={100}
+                    className="h-16 w-16 object-cover"
+                    loader={() => product?.image}
+                  />
+                </td>
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {product.name}
+                </th>
+                <td className="px-6 py-4">{product?.category?.name}</td>
+                <td className="px-6 py-4">{product.brand}</td>
+                <td className="px-6 py-4">{product?.inStock}</td>
+                <td className="px-6 py-4">{product?.price}</td>
+                <td className="px-6 py-4 gap-2 flex">
+                  <DeleteEditButton
+                    token={token}
+                    productId={product?.id}
+                  ></DeleteEditButton>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <nav aria-label="Page navigation example  flexw-full">
+        <ul className="w-full flex items-center -space-x-px h-8 text-sm">
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Previous</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 1 1 5l4 4"
+                />
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              1
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              2
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              aria-current="page"
+              className="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            >
+              3
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              4
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              5
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              <span className="sr-only">Next</span>
+              <svg
+                className="w-2.5 h-2.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default ProductList;
