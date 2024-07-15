bdf9f88ae556505ac690e70d3749b38141c5cc70
Babina081 <babitamoo333@gmail.com>
2024-07-15 22:33:24 +0545

    fix ui malfuction

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -4,7 +4,7 @@ import Charts from "../../../components/admin/Charts";
 
 const DashboardPage = () => {
   return (
-    <section className="m-2  flex flex-col  ">
+    <section className="m-2  flex flex-col mb-8 ">
       <h1 className="text-xl font-bold">Dashboard</h1>
       <div className="grid grid-cols-4 my-4 gap-4">
         <AdminDashCount></AdminDashCount>
@@ -12,11 +12,16 @@ const DashboardPage = () => {
         <AdminDashCount></AdminDashCount>
         <AdminDashCount></AdminDashCount>
       </div>
-      <div className="grid grid-cols-3  gap-4 bg-red-400 ">
-        <Charts></Charts>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-4  ">
+        <div className="border border-gray-300 shadow-lg p-4 rounded-xl">
+          hello
+        </div>
+        <div className="border border-gray-300 shadow-lg p-4 rounded-xl">
+          <Charts></Charts>
+        </div>
+        <div className="border border-gray-300 shadow-lg p-4 rounded-xl">
+          hello
+        </div>
       </div>
     </section>
   );
