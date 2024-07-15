bea5f02d49aa4ceb2d78210fd11e783a5bea3c7e
Babina081 <babitamoo333@gmail.com>
2024-07-15 22:22:39 +0545

    add dashboard contents

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import AdminDashCount from "../../../components/admin/AdminDashCount";
+import Charts from "../../../components/admin/Charts";
 
 const DashboardPage = () => {
-  return <div className="">DashboardPage</div>;
+  return (
+    <section className="m-2  flex flex-col  ">
+      <h1 className="text-xl font-bold">Dashboard</h1>
+      <div className="grid grid-cols-4 my-4 gap-4">
+        <AdminDashCount></AdminDashCount>
+        <AdminDashCount></AdminDashCount>
+        <AdminDashCount></AdminDashCount>
+        <AdminDashCount></AdminDashCount>
+      </div>
+      <div className="grid grid-cols-3  gap-4 bg-red-400 ">
+        <Charts></Charts>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+      </div>
+    </section>
+  );
 };
 
 export default DashboardPage;
