f6565a0b7421ef8b5db7c3918c156c919cabcb3e
Babina081 <babitamoo333@gmail.com>
2024-07-15 22:22:22 +0545

    update overflow property of admin layout oage

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,7 +7,7 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="w-1/4 fixed top-0 left-0">
         <AdminSidebar />
       </div>
-      <div className="flex flex-col w-3/4 fixed top-0 right-0">
+      <div className="flex flex-col w-3/4 fixed top-0 right-0 h-full overflow-auto">
         <AdminNavbar></AdminNavbar>
         {children}
       </div>
